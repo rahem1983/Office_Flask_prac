225ca469833f6462bb2389e45ede96a6a894fb46
rahim1983 <rahimulislam14@gmail.com>
2023-03-20 12:59:13 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import numpy as np
+import cv2
+import pandas as pd
+app = Flask(__name__)
+
+@app.route("/")       
+def hello_world():
+    conda = 23  +    3 
+    return "<p>Hello, World!</p>"
